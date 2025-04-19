460c4c3df91163a210ac68378b0377e4962b4d10
r9dtech <r9dtech@example.com>
2025-04-19 14:18:29 +0100

    feature: allow upload of json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: `
+    <h1>boolder-ukc</h1>
+    <h2>A tool to log boolder climbs on ukc</h2>
+    <p><small>This is probably quite brittle - if UKC change things then it might stop working!</small></p>
+    <form (submit)="$event.preventDefault()">
+      <label>Upload Boolder export:
+        <input type="file" (change)="uploadFile($event)">
+      </label>
+      <pre>
+        {{ boolderExport }}
+      </pre>
+    </form>
+  `,
 })
 export class AppComponent {
   title = 'boolder-ukc';
+
+  boolderExport = "";
+
+  constructor() {
+    console.log("hi");
+  }
+
+  uploadFile(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList?.length === 1) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.boolderExport = reader.result as string
+      }
+      reader.readAsText(fileList[0]);
+    }
+  }
 }
